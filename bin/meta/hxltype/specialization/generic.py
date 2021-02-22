f289ff786fe1b4cc0c44ff7e59b5b872726f00d1
Emerson Rocha <rocha@ieee.org>
2021-02-22 15:42:23 -0300

    hxlquickmeta (#9): hxltype; added GenericSpecializationHtype

---
+++ bin/meta/hxltype/specialization/generic.py
@@ -0,0 +1,23 @@
+# GenericSpecializationHtype
+# @see https://github.com/EticaAI/HXL-Data-Science-file-formats/issues/9
+# TODO: create an entry on the spreadsheet
+
+from dataclasses import dataclass
+
+from typing import (
+    Any
+)
+
+@dataclass(init=True, repr=True, eq=True)
+class GenericSpecializationHtype:
+    """
+    Note: HXL data points does not need to enforce any specialization. This
+    Class can be used to 'reset' other specializations.
+    """
+    specialization: str = None
+
+# TODO: create an somewhat generic class for something that represents
+#       an place (#loc, #country, #adm1-#adm5, +lat, +lon, etc)
+#       @see https://www.humanitarianresponse.info/en/applications/tools/toolbox-item/implementing-p-codes
+#       @see https://www.humanitarianresponse.info/en/operations/stima/document/pcode-matching-tool-excel-add
+#       (fititnt, 2021-02-22 18:35 UTC)
