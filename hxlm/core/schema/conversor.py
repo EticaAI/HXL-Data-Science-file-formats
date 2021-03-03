4255f4837fd92f51984d964905d7e5a749534fc4
Emerson Rocha <rocha@ieee.org>
2021-03-03 01:20:16 -0300

    hxlm (#11): ConversorHSchema class started

---
+++ hxlm/core/schema/conversor.py
@@ -0,0 +1,12 @@
+"""
+
+
+Copyleft 🄯 2021, Emerson Rocha (Etica.AI) <rocha@ieee.org>
+License: Public Domain / BSD Zero Clause License
+SPDX-License-Identifier: Unlicense OR 0BSD
+"""
+
+class ConversorHSchema:
+
+    def __init__(self):
+        self.kind: str = 'ConversorHSchema'
