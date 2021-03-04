102de60a1c1e308de803698c4171ec1cc41a7cc2
Emerson Rocha <rocha@ieee.org>
2021-03-04 10:17:19 -0300

    hxlm (#11): started hxlm.core.schema.urn.HURN class

---
+++ hxlm/core/schema/urn.py
@@ -0,0 +1,32 @@
+"""hxlm.core.schema.urn.HURN
+
+attr.urn:
+ "A Uniform Resource Name (URN) is a Uniform Resource Identifier (URI)
+  that uses the urn scheme. URNs are globally unique persistent
+  identifiers assigned within defined namespaces so they will be
+  available for a long period of time, even after the resource which
+  they identify ceases to exist or becomes unavailable.[1] " -- Wikipedia
+@see https://en.wikipedia.org/wiki/Uniform_Resource_Name
+@see https://www.wikidata.org/wiki/Q76497
+RFC:
+@see https://tools.ietf.org/html/rfc1737
+@see https://tools.ietf.org/html/rfc2141
+Lex prefix by Brazil and Itally
+Example: lexml.gov.br/urn/urn:lex:br:federal:lei:2008-06-19;11705
+@see https://en.wikipedia.org/wiki/Lex_(URN)
+ISO
+@see https://tools.ietf.org/html/rfc5141
+
+
+Copyleft 🄯 2021, Emerson Rocha (Etica.AI) <rocha@ieee.org>
+License: Public Domain / BSD Zero Clause License
+SPDX-License-Identifier: Unlicense OR 0BSD
+"""
+
+
+class HURN:
+    """URN is an abstraction to Uniform Resource Name (URN)
+    """
+
+    def __init__(self):
+        self.kind: str = 'HURN'
