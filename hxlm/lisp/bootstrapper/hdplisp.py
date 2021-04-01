79c43b482048d97617c225787ddafb547959ab10
Emerson Rocha <rocha@ieee.org>
2021-04-01 08:15:21 -0300

    hxlm.lisp drafts: hxlm.lisp.HDPlisp dataclass drafted

--- hxlm/lisp/bootstrapper/hdplisp.py
+++ hxlm/lisp/bootstrapper/hdplisp.py
@@ -7,6 +7,8 @@
 
 >>> parser(ex_add)
 >(add 1 2)
+>>> parser(ex_nest_a)
+>(add 3 (add 1 2))
 
 
 Author: 2021, Emerson Rocha (Etica.AI) <rocha@ieee.org>
@@ -14,30 +16,69 @@ License: Public Domain / BSD Zero Clause License
 SPDX-License-Identifier: Unlicense OR 0BSD
 """
 
+# TODO: use as reference the Racket reference (have more options than averange
+#       lisp-like implementations found on most places), see
+#       https://docs.racket-lang.org/reference/reader.html
+#       (Emerson Rocha, 2021-03-31 18:59 UTC)
 
-def explain(thing: str, level: int = 1):
+from dataclasses import InitVar, dataclass
+
+from typing import (
+    Union
+)
+
+
+@dataclass(repr=False)
+class HDPlisp:
+    """An HDP-like Lisp abstraction class"""
+
+    sexpr: str
+    """The raw initial S-expression"""
+
+    steps: InitVar[list] = []
+    """An somewat Abstract Syntax Tree (needs testing)"""
+
+    token_delimiters = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    }
+    """Non-escaped tokens (, [, { match non-escaped ), ], }
+
+    While '(' and ')' are more common, the HDPlisp users decide others chars
+    to help with organization
+    """
+
+    def __repr__(self):
+        return self.sexpr
+
+
+def explain(thing: Union[str, HDPlisp], level: int = 1):
     """(debug) Just print current pointer
 
     Args:
         thing (str): the information
     """
-    print((">" * level) + thing)
+    print((">" * level) + str(thing))
 
 
 def parser(sexpression: str):
     """Parse an S-expression
 
     Args:
-        sexpression (str): The inputterm
+        sexpression (str): The input term
     """
-    return tokenizer(sexpression)
+    hdplisp = HDPlisp(sexpression)
+    return tokenizer(hdplisp)
 
 
-def reader(token: str):
+def reader(token: HDPlisp):
     """reader..."""
+
     explain(token)
 
 
-def tokenizer(sexpression: str):
+def tokenizer(hdpsexpr: HDPlisp):
     """tokenizer..."""
-    return reader(sexpression)
+    # print(type(hdpsexpr))
+    return reader(hdpsexpr)
