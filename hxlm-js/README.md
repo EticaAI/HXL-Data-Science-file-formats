7bbef57d591e71403e9f4bcf45822e0652501a6d
Emerson Rocha <rocha@ieee.org>
2021-04-04 10:38:25 -0300

    hxlm-js (hdp #18, lisp #18) tests with gpg-sign (#12); fixed usage of page-signer.js (needs '%%%SIGNED_PAGES_PGP_SIGNATURE%%%' on the source page); also changed order of prepare-hxlm-relsease.sh commands

--- hxlm-js/README.md
+++ hxlm-js/README.md
@@ -24,3 +24,55 @@ code) that do have a lot of comments.
   - [hxlm/ontologia/json/core.vkg.json](hxlm/ontologia/json/core.vkg.json)
     - [https://hpd.etica.ai/hxlm/ontologia/json/core.vkg.json](https://hpd.etica.ai/hxlm/ontologia/json/core.vkg.json)
 
+
+
+<!--
+- https://cirw.in/gpg-decoder/
+- https://github.com/tasn/webext-signed-pages
+
+
+## To check GPG signatures (integrity check, not encryption)
+
+> NOTE: this is an **advanced usage** for the worst case scenario were an
+  individual cannot use full-offline processing (like an pre-downloaded app,
+  that as 2021-04-04 did not exist) **AND** (this is important) you did not
+  trust the HTTPS encryption layer or are using VPN (because VPNs have power
+  to break HTTPS).
+
+- Use Chromium or Gecko (firefoxes) variant of this extension
+  - https://github.com/tasn/webext-signed-pages
+- After installing the extension, you will need to manually add (not replace)
+  these configurations, INCLUDING the very specific huge sign key, on the
+  a new field.
+
+### First, check if the extension is working
+
+- This page should return OK/Good green icon
+  - https://stosb.com/~tom/signed-pages/good.html
+- This page should return Not ok, bad, red icon
+  - https://stosb.com/~tom/signed-pages/bad.html
+
+Note that even if the developer pages of the extension did not work, you're
+likely to not make it work here.
+
+
+### To use with on hdp.etica.ai/hxlm-js/
+
+Settings:
+
+Pattern (watch out for html escaping!):
+*://hdp.etica.ai/hxlm-js/*
+*://git.workspace.localhost/EticaAI/HXL-Data-Science-file-formats/hxlm-js/*
+
+
+Pubkey:
+
+Contents from https://www.fititnt.org/gpg/rocha.pub
+
+-->
+
+<!--
+
+Debug GPGs public keys
+
+-->
--- prepare-hxlm-relsease.sh
+++ prepare-hxlm-relsease.sh
@@ -27,6 +27,9 @@
 #      REVISION:  ---
 #===============================================================================
 
+ROOTDIR="$(pwd)"
+# echo "$ROOTDIR"
+
 #### Build JSON Knowledge Graph from YAML ______________________________________
 
 ### YAML to JSON ---------------------------------------------------------------
@@ -37,47 +40,6 @@ yq < hxlm/ontologia/core.vkg.yml > hxlm/ontologia/json/core.vkg.json
 
 # Generate hxlm/ontologia/json/core.lkg.json
 yq < hxlm/ontologia/core.lkg.yml > hxlm/ontologia/json/core.lkg.json
-
-
-#### SHA-384 ___________________________________________________________________
-echo "> SHA-384"
-ROOTDIR="$(pwd)"
-# echo "$ROOTDIR"
-
-### hxlm/ontologia -------------------------------------------------------------
-echo "> > hxlm/ontologia"
-## Create the hashes
-
-cd hxlm/ontologia/json/ || exit
-sha384sum --tag core.lkg.json > core.lkg.json.sum
-sha384sum --tag core.vkg.json > core.vkg.json.sum
-
-## Check the hashes
-sha384sum --check core.lkg.json.sum
-sha384sum --check core.vkg.json.sum
-
-### hxlm-js/ -------------------------------------------------------------------
-echo "> > hxlm-js"
-cd "$ROOTDIR"
-
-## Create the hashes
-
-cd hxlm-js/
-
-# First file, clean old checksums
-rm hxlm-js.sum
-
-
-# Initialize
-sha384sum --tag index.html > hxlm-js.sum
-
-# The rest, append
-sha384sum --tag bootstrapper/hdp-aux.js >> hxlm-js.sum
-sha384sum --tag bootstrapper/hdp-minimam.mjs >> hxlm-js.sum
-sha384sum --tag bootstrapper/hdplisp.js >> hxlm-js.sum
-
-## Check the hashes
-sha384sum --check hxlm-js.sum
 
 #### webext-signed-pages _______________________________________________________
 # @see https://github.com/tasn/webext-signed-pages
@@ -103,11 +65,50 @@ fi
 # npm install minimize
 page_signer="${ROOTDIR}/temp/webext-signed-pages/page-signer.js"
 
-echo "GPG sign of hxlm-js/index.html ..."
+echo "GPG sign of hxlm-js/index.html from hxlm-js/index-src.html ..."
 echo "You will be asked to allow GPG sign your smartcard on this moment"
-echo "${page_signer}" "${ROOTDIR}/hxlm-js/index.html" "${ROOTDIR}/hxlm-js/index-signed.html"
+echo "${page_signer}" "${ROOTDIR}/hxlm-js/index-src.html" "${ROOTDIR}/hxlm-js/index.html"
 
-"${page_signer}" "${ROOTDIR}/hxlm-js/index.html" "${ROOTDIR}/hxlm-js/index-signed.html"
+"${page_signer}" "${ROOTDIR}/hxlm-js/index-src.html" "${ROOTDIR}/hxlm-js/index.html"
+
+#### SHA-384 ___________________________________________________________________
+echo "> SHA-384"
+
+### hxlm/ontologia -------------------------------------------------------------
+echo "> > hxlm/ontologia"
+## Create the hashes
+
+cd hxlm/ontologia/json/ || exit
+sha384sum --tag core.lkg.json > core.lkg.json.sum
+sha384sum --tag core.vkg.json > core.vkg.json.sum
+
+## Check the hashes
+sha384sum --check core.lkg.json.sum
+sha384sum --check core.vkg.json.sum
+
+### hxlm-js/ -------------------------------------------------------------------
+echo "> > hxlm-js"
+cd "$ROOTDIR"
+
+## Create the hashes
+
+cd hxlm-js/
+
+# First file, clean old checksums
+rm hxlm-js.sum
+
+# Initialize
+sha384sum --tag index.html > hxlm-js.sum
+
+# The rest, append
+sha384sum --tag index-src.html > hxlm-js.sum
+sha384sum --tag bootstrapper/hdp-aux.js >> hxlm-js.sum
+sha384sum --tag bootstrapper/hdp-aux.js >> hxlm-js.sum
+sha384sum --tag bootstrapper/hdp-minimam.mjs >> hxlm-js.sum
+sha384sum --tag bootstrapper/hdplisp.js >> hxlm-js.sum
+
+## Check the hashes
+sha384sum --check hxlm-js.sum
 
 #### Additional commands _______________________________________________________
 # cd "$ROOTDIR"
