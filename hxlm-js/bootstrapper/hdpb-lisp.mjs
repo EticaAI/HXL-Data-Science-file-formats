1ecbfdea1de406081361f7e3d3d722f83be474a7
Emerson Rocha <rocha@ieee.org>
2021-04-05 23:09:55 -0300

    hxlm-js (lisp #18): hdpb-lisp.mjs loops (not yet a lot, but loops; still need to convert to recursion or something)

--- hxlm-js/bootstrapper/hdpb-lisp.mjs
+++ hxlm-js/bootstrapper/hdpb-lisp.mjs
@@ -23,6 +23,39 @@ function normalize_input(sxpr) {
 // https://stackoverflow.com/questions/36527642/difference-between-codepointat-and-charcodeat
 
 function parse(sxpr) {
+    let idx = 0
+    let point = sxpr.codePointAt(idx)
+    let level = 0
+    let col = 0
+    let ast = []
+    let ast_bloc = []
+    let token = ''
+    let pointstr_last = ''
+    while (point) {
+        let pointstr = String.fromCodePoint(point)
+        // let pointstr_last = String.fromCodePoint(sxpr.codePointAt(idx))
+        idx += 1
+        if (pointstr === '(') {
+            level += 1
+        }
+        if (pointstr === ')') {
+            ast.push(ast_bloc)
+            ast_bloc = []
+            level -= 1
+        }
+        // TODO: deal with strings and symbols with spaces
+        if (pointstr === ' ') {
+            col += 1
+            // let pointstr_last = String.fromCodePoint(sxpr.codePointAt(idx))
+            ast_bloc.push(pointstr_last.replace('(', '').replace(')', ''))
+            pointstr_last = ''
+            // continue
+        }
+
+        pointstr_last += pointstr.replace(' ', '')
+        console.log(level, col, pointstr, ast_bloc, ast)
+        point = sxpr.codePointAt(idx)
+    }
 
 }
 function parse_blocks(sxpr) {
@@ -47,6 +80,7 @@ class HDPbLisp {
     }
     static just_testing_parser2(sxpr) {
         let sxpr_norm = normalize_input(sxpr)
+        parse(sxpr_norm)
         return sxpr_norm.split(' ')
     }
 }
--- hxlm-js/bootstrapper/testum-hdpb-lisp.mjs
+++ hxlm-js/bootstrapper/testum-hdpb-lisp.mjs
@@ -4,10 +4,23 @@
 
 import { HDPbLisp } from './hdpb-lisp.mjs'
 
-console.log(HDPbLisp.just_testing_parser('( + 1 1 )'))
-console.log(HDPbLisp.just_testing_parser('(+ 1 (- 3 1 ))'))
-console.log(HDPbLisp.just_testing_parser('(+ 1 (- 3 1 ) (+ 1 1 ))'))
+// console.log(HDPbLisp.just_testing_parser('( + 1 1 )'))
+// console.log(HDPbLisp.just_testing_parser('(+ 1 (- 3 1 ))'))
+// console.log(HDPbLisp.just_testing_parser('(+ 1 (- 3 1 ) (+ 1 1 ))'))
 
-console.log(HDPbLisp.just_testing_parser2('( + 1 1 )'))
-console.log(HDPbLisp.just_testing_parser2('(+ 1 (- 3 1 ))'))
-console.log(HDPbLisp.just_testing_parser2('(+ 1 (- 3 1 ) (+ 1 1 ))'))
+// console.log(HDPbLisp.just_testing_parser2('( + 1 1 )'))
+// console.log(HDPbLisp.just_testing_parser2('(+ 1 (- 3 1 ))'))
+
+let example_nil = '()'
+console.log('example_nil', example_nil)
+console.log(HDPbLisp.just_testing_parser2(example_nil))
+console.log('')
+console.log('')
+let example1 = '( + 1 1 )'
+console.log('example1', example1)
+console.log(HDPbLisp.just_testing_parser2(example1))
+console.log('')
+console.log('')
+let example2 = '(+ 1 (- 3 1 ) (+ 1 1 ))'
+console.log('example2', example2)
+console.log(HDPbLisp.just_testing_parser2(example2))
