0c5eacaa2aaa79585ef86eaaf02411bc6ed652eb
Emerson Rocha <rocha@ieee.org>
2021-04-06 13:09:08 -0300

    hxlm-js (lisp #18): hdpb-lisp.mjs less loops, more recursion...

--- hxlm-js/bootstrapper/hdpb-lisp.mjs
+++ hxlm-js/bootstrapper/hdpb-lisp.mjs
@@ -20,6 +20,21 @@ function normalize_input(sxpr) {
     return sxpr
 }
 
+function tokenize_input(sxpr) {
+    // TODO: deal with strings in spaces
+    sxpr = sxpr.replace('[', '(').replace('{', '('); // {} are aliases to ()
+    sxpr = sxpr.replace(']', ')').replace('}', ')'); // [] are aliases to ()
+    sxpr = sxpr.replace('(', ' ( ').replace(')', ' ) ')
+    sxpr = sxpr.split(' ')
+
+    // First and last items of array will be empty string ''. Clean here
+    // TODO: test more this
+    let cleaned = sxpr.filter((v) => v != '')
+    // console.log('sxpr', sxpr)
+    // console.log('cleaned', cleaned)
+    return cleaned
+}
+
 // https://stackoverflow.com/questions/36527642/difference-between-codepointat-and-charcodeat
 
 function parse(sxpr) {
@@ -58,8 +73,43 @@ function parse(sxpr) {
     }
 
 }
-function parse_blocks(sxpr) {
 
+/**
+ * Initial version based on a ported version from http://norvig.com/lispy.html
+ */
+function parse_recursive_ltr(sxpr_token) {
+    if (!sxpr_token || sxpr_token.lenght == 0) {
+        throw "EOF ?"
+    }
+
+    let token = sxpr_token.pop()
+
+    if (token === '(') {
+        parts = []
+        while (sxpr_token[0] !== ')') {
+            parts.push(parse_recursive_ltr())
+        }
+        sxpr_token.pop()
+        return parts
+    } else if (token === ')') {
+        throw ") ?"
+    } else {
+        return atom(sxpr_token)
+    }
+}
+
+function atom(token) {
+    if (Number.isNaN(token)) {
+        return String.toString(token)
+    } else {
+        return Number(token)
+        // if (Number.i(token)) {
+        //     return String.toString(token)
+        // }
+    }
+    // if (Number.isInteger(token)) {
+    //     parseInt(token)
+    // }
 }
 
 /**
@@ -82,6 +132,14 @@ class HDPbLisp {
         let sxpr_norm = normalize_input(sxpr)
         parse(sxpr_norm)
         return sxpr_norm.split(' ')
+    }
+    static just_testing_parser3(sxpr) {
+        let sxpr_norm = normalize_input(sxpr)
+        let sxpr_tokens = tokenize_input(sxpr_norm)
+        console.log('sxpr_tokens', sxpr_tokens)
+        let parsed = parse_recursive_ltr(sxpr_tokens)
+        console.log('parsed', parsed)
+        return parsed
     }
 }
 
--- hxlm-js/bootstrapper/testum-hdpb-lisp.mjs
+++ hxlm-js/bootstrapper/testum-hdpb-lisp.mjs
@@ -4,13 +4,6 @@
 
 import { HDPbLisp } from './hdpb-lisp.mjs'
 
-// console.log(HDPbLisp.just_testing_parser('( + 1 1 )'))
-// console.log(HDPbLisp.just_testing_parser('(+ 1 (- 3 1 ))'))
-// console.log(HDPbLisp.just_testing_parser('(+ 1 (- 3 1 ) (+ 1 1 ))'))
-
-// console.log(HDPbLisp.just_testing_parser2('( + 1 1 )'))
-// console.log(HDPbLisp.just_testing_parser2('(+ 1 (- 3 1 ))'))
-
 let example_nil = '()'
 console.log('example_nil', example_nil)
 console.log(HDPbLisp.just_testing_parser2(example_nil))
@@ -35,3 +28,18 @@ console.log('')
 let example_str2 = '(🇧🇷 (⚕️ "hospital"))'
 console.log('example_str2', example_str2)
 console.log(HDPbLisp.just_testing_parser2(example_str2))
+
+console.log('')
+console.log('')
+console.log('just_testing_parser3')
+
+console.log('')
+console.log('')
+console.log('')
+
+// let example_str2 = '(🇧🇷 (⚕️ "hospital"))'
+console.log('example1', example1)
+console.log(HDPbLisp.just_testing_parser3(example1))
+console.log('')
+// console.log('example_str2', example_str2)
+// console.log(HDPbLisp.just_testing_parser3(example_str2))
