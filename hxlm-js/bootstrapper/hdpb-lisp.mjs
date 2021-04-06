d0e20633ab467af5437210168cab7f8330cd9a4f
Emerson Rocha <rocha@ieee.org>
2021-04-06 16:18:52 -0300

    hxlm-js (lisp #18): HDPbLisp.{ast,ast_ltr,ast_rtl}() started; Right-to-left? (like Imperial Aramaic, not Lingua Latina) will need some serious work on abstract syntax tree

--- hxlm-js/bootstrapper/hdpb-lisp.mjs
+++ hxlm-js/bootstrapper/hdpb-lisp.mjs
@@ -78,7 +78,7 @@ function parse(sxpr) {
 
 }
 
-const _HDP_DEBUG = typeof(HDP_DEBUG) !== 'undefined' && HDP_DEBUG || false
+const _HDP_DEBUG = typeof (HDP_DEBUG) !== 'undefined' && HDP_DEBUG || false
 // const _HDP_DEBUG = typeof (HDP_DEBUG) !== 'undefined' && HDP_DEBUG || true
 
 /**
@@ -155,20 +155,101 @@ class HDPbLisp {
     version = "0.8.5"
 
 
-    static ast(sxpr) {
-
+    /**
+     * HDPLisp Abstract syntax three with RTL format
+     *
+     * Trivia:
+     *  - https://en.wikipedia.org/wiki/Right-to-left
+     *  - https://en.wikipedia.org/wiki/Left-to-right
+     *     - as 2021-04-06, this does not _even_ have dedicated Wikipedia page
+     *
+     * @param   {String}   sxpr      Input string
+     * @param   {Object}   optionem  Object with booleans to explicit define
+     *                               the source input
+     *                               {
+     *                                  lrt: false,
+     *                                  rlt: true
+     *                               }
+     *                               ltr: Left-to-right? (like Lingua Latina?)
+     *                               rtl: Right-to-left? (like Imperial Aramaic?)
+     * @returns {Array}
+     */
+    static ast(sxpr, optionem) {
+        // As a way of paying tribute to Arabic numbers, if a user does not
+        // specify which AST he wants, we will provide the RTL.
+        // As of today (2021-04-06) there is also an interest that hashing
+        // algorithms also use RTL as a reference.
+        // (Emerson Rocha, 2021-04-06 18:37 UTC)
+        return this.ast_rtl(sxpr, optionem)
     }
 
+    /**
+     * HDPLisp Abstract syntax three with LTR format
+     *
+     * @param   {String}   sxpr      Input string
+     * @param   {Object}   optionem  Object with booleans to explicit define
+     *                               the source input
+     *                               {
+     *                                  lrt: false,
+     *                                  rlt: true
+     *                               }
+     *                               ltr: Left-to-right? (like Lingua Latina?)
+     *                               rtl: Right-to-left? (like Imperial Aramaic?)
+     * @returns {Array}
+     */
+    static ast_ltr(sxpr, optionem) {
+        let res = tokenize_input(sxpr)
+        // TODO: implement optionem. In special if the initial token is not
+        //       already on the text directin the user wants
+        let resultatum = parse_recursive_ltr(res)
+        return resultatum
+    }
 
+    /**
+     * HDPLisp Abstract syntax three with RTL format
+     *
+     * Trivia:
+     *  - https://en.wikipedia.org/wiki/Right-to-left
+     *  - https://en.wikipedia.org/wiki/Left-to-right
+     *     - as 2021-04-06, this does not _even_ have dedicated Wikipedia page
+     *
+     * @param   {String}   sxpr      Input string
+     * @param   {Object}   optionem  Object with booleans to explicit define
+     *                               the source input
+     *                               {
+     *                                  lrt: false,
+     *                                  rlt: true
+     *                               }
+     *                               ltr: Left-to-right? (like Lingua Latina?)
+     *                               rtl: Right-to-left? (like Imperial Aramaic?)
+     * @returns {Array}
+     */
+    static ast_rtl(sxpr, optionem) {
+
+        // TODO: ast_rtl
+        return this.ast_ltr(sxpr, optionem)
+    }
+
+    /**
+     * @deprecated to be removed
+     */
     static just_testing_parser(sxpr) {
         let sxpr_norm = normalize_input(sxpr)
         return sxpr_norm.split(' ')
     }
+
+    /**
+     * @deprecated to be removed
+     */
     static just_testing_parser2(sxpr) {
         let sxpr_norm = normalize_input(sxpr)
         parse(sxpr_norm)
         return sxpr_norm.split(' ')
     }
+
+    /**
+     * @deprecated to be removed
+     */
     static just_testing_parser3(sxpr) {
         // console.log('just_testing_parser3', sxpr)
         let sxpr_norm = normalize_input(sxpr)
---
+++ tests/hdplisp/salve-mundi/README.md
@@ -0,0 +1,24 @@
+# tests/hdplisp/salve-mundi/
+
+> This directory is an working draft (Emerson Rocha, 2021-04-06 19:06 UTC)
+
+## .qqq.hpdlisp.json
+
+- [.qqq.hpdlisp.json](.qqq.hpdlisp.json)
+
+### About `qqq`
+- <https://en.wikipedia.org/wiki/ISO_639-3>
+  - <https://en.wikipedia.org/wiki/Constructed_language>
+  - <https://www.kreativekorp.com/clcr/>
+
+Reason:
+ - ISO 639-3 allow usage from qaa-qtz for user defined languages
+ - `qqq` is free
+ - The idea behind `.qqq.hpdlisp.json` is be an
+   [Abstract Syntax Tree](https://en.wikipedia.org/wiki/Abstract_syntax_tree)
+   and from the qaa-qtz namespace, qqq seems to be the most extreme naming for
+   an constructed language _ever_.
+
+## TODOs
+- https://askubuntu.com/questions/522512/switch-text-direction-in-gedit
+- https://askubuntu.com/questions/380776/stabilizing-characters-like-and
