ffb6e7b82daa8176e1f664cf6dca7ab908e83352
Emerson Rocha <rocha@ieee.org>
2021-04-13 00:22:06 -0300

    hdplisp (#18), i18n+l10n (#15): prototyping. #lang hdpl/radix; draft of terms that could be enforced as fallback on any localization

--- hdpl-conventions/proof-of-concept/racket-lang/testum.rkt
+++ hdpl-conventions/proof-of-concept/racket-lang/testum.rkt
@@ -1,6 +1,7 @@
-#lang hdpl/linguam/mis-Qaaa
-; #lang hdpl/linguam/lat-Latn
+#lang hdpl/linguam/lat-Latn
+; 
 ; #lang hdpl
 ; "testing hdpl/linguam/lat-Latn now just to see if it works https://docs.racket-lang.org/guide/language-collection.html"
 
 (+ 1 2 3)
+
