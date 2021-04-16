b26eb727a4a292f294b1f58fe01ddc7d394e9799
Emerson Rocha <rocha@ieee.org>
2021-04-16 01:55:44 -0300

    hdplisp-on-racket (#18): code refactoring

--- hdpl-conventions/proof-of-concept/racket-lang/un/testum-cod-services.eng.rkt
+++ hdpl-conventions/proof-of-concept/racket-lang/un/testum-cod-services.eng.rkt
@@ -13,138 +13,15 @@
 (uk.org.redcross->ab "BGD")
 
 ; servitium-org-geoboundaries
-(servitium-org-geoboundaries->patriam "AGO")
+(servitium/org-geoboundaries->patriam "AGO")
 ; (servitium-org-geoboundaries->patriam)
 
 
-(define Example1 (force servitium-org-geoboundaries))
-
-; (for ([item (force Example1)] )
-;   (for (((key val) (in-hash item)))
-;     (printf "~a = ~a~%" key val))
-; )
-
-
-;(define test-ago
-;  (cond
-;    (empty? 
-;
-;  )
-
-;(filter  Example1]
-;
-;  (display item)
-;  )
-
-
-;(for/list [item Example1]
-;  (print item)
-;  )
-
-; (apply println servitium-org-geoboundaries)
+(define Example1 (force servitium/org-geoboundaries))
 
 ; https://beta.itos.uga.edu/CODV2API/api/v1/locations/mng
 
-(length (list "hop" "skip" "jump"))
-
-(list-ref (list "hop" "skip" "jump") 0) 
-
-(member "fall" (list "hop" "skip" "jump")) 
-
-
-(map sqrt (list 1 4 9 16))
-
-
-(map (lambda (i)
-       (string-append i "!"))
-     (list "peanuts" "popcorn" "crackerjack"))
-
-
-(filter string? (list "a" "b" 6))
-
-(filter positive? (list 1 -2 6 7 0))
-
-
-(cons "head" empty)
-
-
-(cons "dead" (cons "head" empty))
-
-
-;(define (my-length lst)
-;  (cond
-;   [(empty? lst) 0]
-;   [else (+ 1 (my-length (rest lst)))]))
-
-
-
-;(define (my-map f lst)
-;  (cond
-;   [(empty? lst) empty]
-;   [else (cons (f (first lst))
-;               (my-map f (rest lst)))]))
-;
-;
-;(my-length (list "a" "b" "c"))
-
-(define (my-length lst)
-  ; local function iter:
-  (define (iter lst len)
-    (cond
-      [(empty? lst) len]
-      [else (iter (rest lst) (+ len 1))]))
-  ; body of my-length calls iter:
-  (iter lst 0))
-
-
-(my-length (list "a" "b" "c"))
-
-
-
-;(define (my-map f lst)
-;  (define (iter lst backward-result)
-;    (cond
-;     [(empty? lst) (reverse backward-result)]
-;     [else (iter (rest lst)
-;                 (cons (f (first lst))
-;                       backward-result))]))
-;  (iter lst empty))
-
-(define (my-map f lst)
-  (for/list ([i lst])
-    (f i)))
-
-;
-;(filter string? Example1)
-
-;(filter (hash-has-key? v "AGO") Example1)
-
-;(define (my-map f lst)
-;  (define (iter lst backward-result)
-;    (cond
-;     [(empty? lst) (reverse backward-result)]
-;     [else (iter (rest lst)
-;                 (cons (f (first lst))
-;                       backward-result))]))
-;  (iter lst empty))
-
-(for/list ([i '(1 2 3)]
-           [j "abc"]
-           #:when (odd? i)
-           [k #(#t #f)])
-  (list i j k))
-
-
-
-
-(for/hash ([i '(1 2 3)])
-  (values i (number->string i)))
-
-;; @NlightNFotis: "A small naive implementation of map, filter and reduce in racket, implemented by myself."
-; https://gist.github.com/NlightNFotis/b662a0368b5eea68ebfde1e4e4fb9787
-
-
-(define  [hash-have-this-key? hash key val]
+(define [hash-have-this-key? hash key val]
   ;  (print hash))
   ;  (hash-keys hash))
 
@@ -157,26 +34,21 @@
 
 
 
-(first Example1)
-(first (hash-keys (first Example1)))
-'boundaryISO
-
-(hash-have-this-key? (first Example1) 'boundaryISO "AFG")
+;(first Example1)
+;(first (hash-keys (first Example1)))
+;'boundaryISO
+;
+;(hash-have-this-key? (first Example1) 'boundaryISO "AFG")
 
 ;; Note to self: hash keys are symbols, but values tend to be string/number
 
 
-(define [servitium-org-geoboundaries->patriam_test patriam]
+;; TODO: remove this later
+(define [servitium/org-geoboundaries->patriam_test patriam]
   ; uses external Example1
 
-;  ; local function iter: (almost there...)
-;  (define [iter lst resultum]
-;    (cond
-;      [(empty? lst) resultum]
-;      [else (iter (rest lst) (cons resultum 1))]))
-  
 
-  ; local function iter: (almost there...)
+  ; local function iter:
   (define [iter lst resultum]
     (cond
       [(empty? lst) resultum]
@@ -191,16 +63,19 @@
   (iter Example1 '()))
 
 (println "testing AFG...")
-(servitium-org-geoboundaries->patriam_test "AFG")
+;(servitium/org-geoboundaries->patriam_test "AFG")
+;(servitium/org-geoboundaries->index "AFG")
+(force (servitium/org-geoboundaries->index "AFG"))
+
 
 (println "testing BRA...")
-(servitium-org-geoboundaries->patriam_test "BRA")
-
+;(servitium/org-geoboundaries->patriam_test "BRA")
+(force (servitium/org-geoboundaries->index "BRA"))
 
 
 (println "testing non-existent like XPTO...")
-(servitium-org-geoboundaries->patriam_test "XPTO")
-;(servitium-org-geoboundaries->patriam_test "BRA222")
+;(servitium/org-geoboundaries->patriam_test "XPTO")
+(force (servitium/org-geoboundaries->index "XPTO"))
 
 ;(define [servitium-org-geoboundaries->patriam_test patriam]
 ;  (for/fold ([acc '()]
--- hdpl-conventions/prototype/hdpl/commune/servitium.rkt
+++ hdpl-conventions/prototype/hdpl/commune/servitium.rkt
@@ -1,4 +1,5 @@
-#lang lazy
+#lang racket
+;#lang lazy
 ; #lang racket
 ;; File: hdpl/commune/servitium.rkt
 ;; Author: 2021, Emerson Rocha (Etica.AI) <rocha@ieee.org>
@@ -7,10 +8,13 @@
 ;;
 ;; Trivia
 ;; - "servitium"
-;;  - https://en.wiktionary.org/wiki/servitium#Latin
+;;    - https://en.wiktionary.org/wiki/servitium#Latin
 ;; - "dictiōnārium"
-;;  - https://en.wiktionary.org/wiki/dictionarium#Latin
-
+;;    - https://en.wiktionary.org/wiki/dictionarium#Latin
+;; - "indicem"
+;;    - https://en.wiktionary.org/wiki/index#Latin
+;; - "praecīsiōnem"
+;;    - https://en.wiktionary.org/wiki/praecisio#Latin
 
 ;; TODO: considere this as option to implement lazy evaluation
 ;;       http://matt.might.net/articles/implementing-laziness/
@@ -21,31 +25,21 @@
 
 (require json)
 (provide
-  ServitumDictionarium
-  ; GeoboundariesOrgServitumDictionarium
-; )
-  servitium-org-geoboundaries
-  servitium-org-geoboundaries->patriam
-  )
+ ServitumDictionarium
+ ; GeoboundariesOrgServitumDictionarium
+ ; )
+ servitium/org-geoboundaries
+ servitium/org-geoboundaries->patriam
+ servitium/org-geoboundaries->index
+ )
 
 
-;; http://matt.might.net/articles/implementing-laziness/
-; (define-syntax (define/by-need stx)
-;   (syntax-case stx ()
-;     [(_ (f v ...) body ...)
-;      (with-syntax ([($f) (generate-temporaries #'(f))])
-;        #'(begin
-;            (define-syntax f
-;              (syntax-rules ()
-;                [(_ arg (... ...))
-;                 ($f (delay arg) (... ...))]))
-;            (define ($f v ...)
-;              (let-syntax ([v (lambda (stx*)
-;                                (syntax-case stx* ()
-;                                  [(_ arg (... ...)) 
-;                                   #'((force v) arg (... ...))]
-;                                  [_ #'(force v)]))] ...)
-;                body ...))))]))
+
+;; Simple local helper
+(define [hash-have-this-key? hash key val]
+  (and
+   [hash-has-key? hash key]
+   [equal? (hash-ref hash key) val]))
 
 
 (define ServitumDictionarium
@@ -57,29 +51,51 @@
 
 (define GeoboundariesOrgServitumDictionarium
   (lazy
-    (read-json
-      (get-pure-port (string->url "https://www.geoboundaries.org/gbRequest.html?ISO=ALL")))))
+   (read-json
+    (get-pure-port (string->url "https://www.geoboundaries.org/gbRequest.html?ISO=ALL")))))
 
 
 ; Return list raw list of values
-(define servitium-org-geoboundaries
+(define servitium/org-geoboundaries
   GeoboundariesOrgServitumDictionarium)
 
+
+
+(define [servitium/org-geoboundaries->index patriam [praecisionem empty] ]
+  ; Note: uses GeoboundariesOrgServitumDictionarium
+
+  ; TODO: implement also ADM level praecisionem
+
+  ; local function iter:
+  (define [iter lst resultum]
+    (cond
+      [(empty? lst) resultum]
+      [else 
+       (iter (rest lst)
+             (cond
+               [(hash-have-this-key? (first lst) 'boundaryISO patriam) (cons resultum (first lst))]
+               [else resultum]
+               ))]))
+
+  ; body calls iter:
+  (iter (force GeoboundariesOrgServitumDictionarium) '()))
+
+
 ; List of ISO3 country codes
-(define [servitium-org-geoboundaries->patriam patriam]
-  (lazy "TODO: servitium-org-geoboundaries->patriam"))
+(define [servitium/org-geoboundaries->patriam patriam]
+  (lazy "TODO: servitium/org-geoboundaries->patriam"))
 
 ; Does this country have any information?
-(define servitium-org-geoboundaries->patriam?
-  "TODO: servitium-org-geoboundaries->patriam?")
+(define servitium/org-geoboundaries->patriam?
+  "TODO: servitium/org-geoboundaries->patriam?")
 
 ; Does this country have an adm1 level? (Note: all that do exist, have at least adm0)
-(define servitium-org-geoboundaries->patriam-adm1?
-  "TODO: servitium-org-geoboundaries->patriam-adm1?")
+(define servitium/org-geoboundaries->patriam-adm1?
+  "TODO: servitium/org-geoboundaries->patriam-adm1?")
 
 ; Does this country have an adm2 level? (Note: all that do exist, have at least adm0)
-(define servitium-org-geoboundaries->patriam-adm2?
-  "TODO: servitium-org-geoboundaries->patriam-adm2?")
+(define servitium/org-geoboundaries->patriam-adm2?
+  "TODO: servitium/org-geoboundaries->patriam-adm2?")
 
 ; TODO: other levels
 
