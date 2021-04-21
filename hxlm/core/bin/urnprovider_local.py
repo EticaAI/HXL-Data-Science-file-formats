4d9e25c24b361490008de2e0b6413dcc969fba8d
Emerson Rocha <rocha@ieee.org>
2021-04-21 01:44:34 -0300

    urnresolver (#13): urnprovider-local draft

---
+++ hxlm/core/bin/urnprovider_local.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# ==============================================================================
+#
+#          FILE:  urnprovider_local.py
+#
+#         USAGE:  urnprovider-local (...)
+#
+#   DESCRIPTION:  ---
+#
+#       OPTIONS:  ---
+#
+#  REQUIREMENTS:  - python3
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Emerson Rocha <rocha[at]ieee.org>
+#       COMPANY:  Etica.AI
+#       LICENSE:  Public Domain dedication
+#                 SPDX-License-Identifier: Unlicense
+#       VERSION:  v0.1
+#       CREATED:  2021-04-21 04:21 UTC v0.1 draft
+# ==============================================================================
+
+
+def exec_from_console_scripts():
+    """[summary]
+    """
+    print("{}")
